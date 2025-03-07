0c16e06337e6e89fa7b2d30df518d9c2afcff4d8
jing <n112140@gmail.com>
2025-03-07 15:19:52 +0800

    新增article頁面

---
+++ app/articles/_components/articles-banner.js
@@ -0,0 +1,14 @@
+'use client'
+
+import articlecss from '../styles/articles.module.css'
+
+export default function ArticlesBanner() {
+  return (
+    <>
+      <div className={articlecss.banner}>
+        <h3>GYM享知識</h3>
+        {/* <p>尋找有相同運動愛好的朋友，一起GYM步吧！</p> */}
+      </div>
+    </>
+  )
+}
---
+++ app/articles/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ArticlesBanner from './_components/articles-banner'
+export default function ArticlesPage() {
+  return (
+    <>
+    <ArticlesBanner/>
+
+    </>
+  )
+}
