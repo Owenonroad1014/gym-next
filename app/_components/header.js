037ed773db04c2f10964784203e0cc07bf6e7ead
PAO <4A61C003@stust.edu.tw>
2025-03-09 22:12:27 +0800

    修改登入、註冊路徑

--- app/_components/header.js
+++ app/_components/header.js
@@ -61,12 +61,12 @@ export default function Header() {
           </form> */}
 
           {/* 登入按鈕 */}
-          <Link href="/login" className={headerstyles.navLink}>
+          <Link href="/member/login" className={headerstyles.navLink}>
             登入
           </Link>
 
           {/* 註冊按鈕 */}
-          <Link href="/register" className={headerstyles.navLink}>
+          <Link href="member/register" className={headerstyles.navLink}>
             註冊
           </Link>
           <Link href="/cart" className={headerstyles.navLink}>
