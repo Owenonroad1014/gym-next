7ea2517929f7fc828fc1487a97c347fb1295b2ee
ching <s22h20@gmail.com>
2025-03-14 14:16:27 +0800

    更新商品頁面

--- app/products/components/ProductGrid.js
+++ app/products/components/ProductGrid.js
@@ -2,110 +2,63 @@ import React from 'react'
 import ProductCard from './ProductCard'
 import styles from './_styles/ProductGrid.module.css'
 import Pagination from "./Pagination";
-import { useState } from 'react';
+import { useState, useEffect} from "react";
 import Sort from "./sort";
+import { PRODUCTS_LIST , IMG_PATH} from "@/config/api-path";
+import { useRouter, useSearchParams } from "next/navigation";
 
 
-const products = [
-  {
-    id: 1,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 2,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 3,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 4,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 5,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 6,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 7,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  },
-  {
-    id: 8,
-    name: '拳擊手套',
-    price: '45元/天',
-    description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
-    image:
-      "/img/products.jpg",
-    icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
-    variant: 'dark',
-  }
-]
+
+
+// const products = [
+//   {
+//     id: 1,
+//     name: '拳擊手套',
+//     price: '45元/天',
+//     description: '高品質拳擊手套，適合訓練或比賽。高品質拳擊手套，適合訓練或比賽',
+//     image:
+//       "/img/products.jpg",
+//     icon: 'https://cdn.builder.io/api/v1/image/assets/TEMP/f88f917d01813c0c86813110ff466a6d338f1aec9e7e6c90992cb541f1431c21?placeholderIfAbsent=true',
+//     variant: 'dark',
+//   },
+
 
 const ProductGrid = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 5;
+
+  const searchParams = useSearchParams();
+
+  const [Products, setProducts] = useState({
+    success: false,
+    perPage: 0,
+    totalRows: 0,
+    totalPages: 0,
+    page: 0,
+    rows: [],
+    keyword: "",
+  });
+  
+  useEffect(() => {
+    fetch(`${PRODUCTS_LIST}${location.search}`)
+    .then((r) => r.json())
+      .then((obj) => {
+        console.log(obj);
+        if (obj.success) {
+          setProducts(obj);
+        }
+      });
+  }, [searchParams]);
+
   return (
     <section className={styles.productGrid}>
           <Sort />
           <div className={styles.productArea}>
           <div className={styles.productItem}>
-              {products.map((product) => (
+              {Products.rows.map((product) => (
                 <ProductCard key={product.id} {...product} />
               ))}
               </div>
               <div>
-                    <Pagination
-                      currentPage={currentPage}
-                      totalPages={totalPages}
-                      onPageChange={setCurrentPage}
+                    <Pagination {...Products}
                     />
               </div>
 
--- app/products/product-detail/_components/RelatedProducts.js
+++ app/products/product-detail/_components/RelatedProducts.js
@@ -1,9 +1,61 @@
 import React from "react";
 import styles from "./_styles/ProductDetail.module.css";
 import ProductCard from "./ProductCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
 
 const RelatedProducts = () => {
+  // function SampleNextArrow(props) {
+  //   const { className, style, onClick } = props;
+  //   return (
+  //     <div
+  //       className={classname}
+  //       style={{ ...style, display: "block", background: "#f87808",borderRadius: "50%" }}
+  //       onClick={onClick}
+  //     />
+  //   );
+  // }
+  
+  // function SamplePrevArrow(props) {
+  //   const { className, style, onClick } = props;
+  //   return (
+  //     <div
+  //       className={className}
+  //       style={{ ...style, display: "block", background: "#f87808" ,borderRadius: "50%"}}
+  //       onClick={onClick}
+  //     />
+  //   );
+  // }
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  }
   const products = [
     {
       id: 1,
@@ -44,11 +96,13 @@ const RelatedProducts = () => {
 
   return (
     <section className={styles.relatedProducts}>
-      <div className={styles.relatedProductsGrid}>
+    <Slider {...settings}>
       {products.map((product) => (
+        <div key={product.id} className={styles.slideItem}>
                 <ProductCard key={product.id} {...product} />
+        </div>
               ))}
-      </div>
+      </Slider>      
     </section>
   );
 };
