e11e2c9b1518f34effa913b41a52431809f816ea
jing <n112140@gmail.com>
2025-03-14 15:39:29 +0800

    add article api

--- config/api-path.js
+++ config/api-path.js
@@ -1,42 +1,41 @@
-
-export const API_SERVER = `http://localhost:3001`;
-
+export const API_SERVER = `http://localhost:3001`
 
 // 頭貼的路
-export const AVATAR_PATH = `${API_SERVER}/imgs`;
+export const AVATAR_PATH = `${API_SERVER}/imgs`
 
 // 取得教練的資料 GET
-export const COACHES_LIST = `${API_SERVER}/coaches/api`;
+export const COACHES_LIST = `${API_SERVER}/coaches/api`
 
 // 通訊錄: 取得單一項目資料
 // `${API_SERVER}/address-book/api/${ab_id}`
-export const AB_ITEM_GET = `${API_SERVER}/address-book/api`;
-
+export const AB_ITEM_GET = `${API_SERVER}/address-book/api`
 
 // 通訊錄: 新增資料 POST
-export const AB_ADD_POST = `${API_SERVER}/address-book/api`;
+export const AB_ADD_POST = `${API_SERVER}/address-book/api`
 
 // 通訊錄: 刪除項目 DELETE
 // `${API_SERVER}/address-book/api/${ab_id}`
-export const AB_DELETE = `${API_SERVER}/address-book/api`;
+export const AB_DELETE = `${API_SERVER}/address-book/api`
 
 // 通訊錄: 修改項目 PUT
 // `${API_SERVER}/address-book/api/${ab_id}`
-export const AB_ITEM_PUT = `${API_SERVER}/address-book/api`;
-
-
+export const AB_ITEM_PUT = `${API_SERVER}/address-book/api`
 
 // JWT 登入
-export const JWT_LOGIN_POST = `${API_SERVER}/login-jwt`;
+export const JWT_LOGIN_POST = `${API_SERVER}/login-jwt`
 
 // Toggle like
 // `${API_SERVER}/address-book/toggle-like/${ab_id}`
-export const TOGGLE_LIKE = `${API_SERVER}/address-book/toggle-like`;
-
+export const TOGGLE_LIKE = `${API_SERVER}/address-book/toggle-like`
 
 // export const API_SERVER = `http://localhost:3001`;
 
-export const IMG_PATH = `${API_SERVER}/img`;
+export const IMG_PATH = `${API_SERVER}/img`
 
 // 取得商品列表的資料 GET
-export const PRODUCTS_LIST = `${API_SERVER}/products/api`;
+export const PRODUCTS_LIST = `${API_SERVER}/products/api`
+
+// 獲取文章列表的資料
+export const ARTICLES_LIST = `${API_SERVER}/api/articles`
+// 獲取單一文章列表的資料 `${API_SERVER}/api/articles/:articleid`
+export const ARTICLE_ITEM = `${API_SERVER}/api/articles`
