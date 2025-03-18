6f96004815266a522f4395cd16fcdbe5044e6efb
jing <n112140@gmail.com>
2025-03-18 15:27:37 +0800

    更新交友邀請、聊天室頁面、文章列表及gym友

--- app/articles/[articleid]/_components/share-sidebar.js
+++ app/articles/[articleid]/_components/share-sidebar.js
@@ -1,10 +1,86 @@
 'use client'
 import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import articleStyle from '../article.module.css'
 import { FaList, FaRegHeart } from 'react-icons/fa'
 import { IoShareSocialSharp } from 'react-icons/io5'
-export default function ShareSidebar() {
+import Link from 'next/link'
+import Swal from 'sweetalert2'
+
+export default function ShareSidebar({
+  articles = {
+    category_id: 0,
+    content: '',
+    created_at: '',
+    id: 0,
+    imageURL: '',
+    intro: '',
+    title: '',
+    updated_at: '',
+    views: 0,
+  },
+}) {
+  const router = useRouter()
+  const Swal = require('sweetalert2')
   const [isShow, setIsShow] = useState(false)
+  const [like, setLike] = useState(articles.like_id || false)
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: false,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer
+      toast.onmouseleave = Swal.resumeTimer
+    },
+  })
+
+  const toggleLike = (e, article_id) => {
+    e.preventDefault()
+
+    fetch(`${ARTICLE_FAV}/${article_id}`, { headers: { ...getAuthHeader() } })
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result)
+        if (result.success) {
+          // setRefresh(!refresh)//充新抓資料
+          //另一種作法
+          const newListData = structuredClone(listData)
+          newListData.rows.forEach((r) => {
+            if (r.id == result.article_id) {
+              r.like_id = result.action == 'add' ? true : false
+            }
+          })
+          setListData(newListData)
+        }
+      })
+      .catch((error) => {
+        console.error('Error while updating favorite status:', error)
+      })
+  }
+
+  const copyUrl = () => {
+    // 獲取當前頁面的 URL
+    const url = window.location.href
+
+    // 使用 clipboard API 複製 URL 到剪貼簿
+    navigator.clipboard.writeText(url).then(
+      () => {
+        Toast.fire({
+          icon: 'success',
+          title: '複製成功!',
+        })
+      },
+      (err) => {
+        Toast.fire({
+          icon: 'warning',
+          title: '複製網址時發生錯誤',
+        })
+      }
+    )
+  }
+
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY
@@ -22,6 +98,7 @@ export default function ShareSidebar() {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+
   return (
     <>
       <div
@@ -31,16 +108,26 @@ export default function ShareSidebar() {
       >
         <ul className={articleStyle.wrapper}>
           <li className={articleStyle.icon}>
-            <span className={articleStyle.tooltip}>菜單</span>
-            <FaList />
+            <Link href="/articles">
+              <span className={articleStyle.tooltip}>菜單</span>
+              <FaList />
+            </Link>
           </li>
-          <li className={articleStyle.icon}>
+          <li className={articleStyle.icon} onClick={copyUrl}>
             <span className={articleStyle.tooltip}>分享</span>
             <IoShareSocialSharp />
           </li>
+
           <li className={articleStyle.icon}>
             <span className={articleStyle.tooltip}>收藏</span>
-            <FaRegHeart />
+            {like ? (
+              <FaHeart
+                className={articleStyle.heartActive}
+                onClick={(e) => toggleLike(e, articles.id)}
+              />
+            ) : (
+              <FaRegHeart onClick={(e) => toggleLike(e, articles.id)} />
+            )}
           </li>
         </ul>
       </div>
