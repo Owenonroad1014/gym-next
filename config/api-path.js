ff2e062e5e2d25112042525919e2f76bf51b0d27
jing <n112140@gmail.com>
2025-03-18 15:14:44 +0800

    ARTICLE、FRIEND、CHAT api新增

--- config/api-path.js
+++ config/api-path.js
@@ -47,3 +47,38 @@ export const IMG_PATH = `${API_SERVER}/img`;
 
 // 取得商品列表的資料 GET
 export const PRODUCTS_LIST = `${API_SERVER}/products/api`;
+
+//**************** ARTICLE *****************
+// 獲取文章列表的資料
+export const ARTICLES_LIST = `${API_SERVER}/articles/api`
+// 獲取單一文章列表的資料 `${API_SERVER}/api/articles/:articleid`
+export const ARTICLE_ITEM = `${API_SERVER}/articles/api`
+// 我的最愛 ${API_SERVER}/articles/api/toggle-likes/:article_id
+export const ARTICLE_FAV = `${API_SERVER}/articles/api/toggle-likes`
+
+// TOP5
+export const ARTICLE_TOP5 = `${API_SERVER}/articles/api/top-five`
+// Reccomand /articles/api/recommand/1
+export const ARTICLE_RECOMMAND = `${API_SERVER}/articles/api/recommand`
+
+//**************** FRIEND  *****************
+// 獲取好友列表
+export const FRIEND_LIST = `${API_SERVER}/friends/api`
+// 獲取好友邀請列表
+export const FRIEND_REQ_LIST = `${API_SERVER}/friends/api/invite`
+// 刪除好友
+export const FRIEND_DELETE = `${API_SERVER}/friends/api/delete`
+// 好友請求api/request
+export const FRIEND_REQUEST = `${API_SERVER}/friends/api/request`
+// 接受好友邀請api
+export const FRIEND_ACCEPT = `${API_SERVER}/friends/api/accept`
+// 拒絕好友邀請api
+export const FRIEND_REJECT = `${API_SERVER}/friends/api/reject`
+
+//**************** CHAT  *****************
+// 獲取聊天室列表
+export const CHATS_LIST = `${API_SERVER}/chats/api`
+// 獲取指定單一聊天室 chats/api/:chatroomid
+export const CHATS_ITEM = `${API_SERVER}/chats/api`
+// 獲取聊天室訊息
+export const CHATS_MSG = `${API_SERVER}/chats/api/chatroom/1`
