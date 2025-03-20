5898a3246f0744a5eed1c46c796dd29f8f82a3a2
PAO <4A61C003@stust.edu.tw>
2025-03-20 20:56:28 +0800

    資料庫更新-member_profile新增填寫狀態欄位

--- database/member.sql
+++ database/member.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-03-20 02:06:56
+-- 產生時間： 2025-03-20 13:54:43
 -- 伺服器版本： 8.0.40
 -- PHP 版本： 8.2.12
 
@@ -42,16 +42,16 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`member_id`, `name`, `email`, `password_hash`, `password_g`, `created_at`, `update_at`) VALUES
-(1, '陳小明', 'owen@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 17:43:45', '2025-03-18 18:09:15'),
-(2, '林一美', 'jing@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 17:43:45', '2025-03-18 18:09:20'),
-(3, '劉秀杰', 'hsin@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 17:43:45', '2025-03-18 18:09:25'),
-(4, '王名強', 'ching@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 17:43:45', '2025-03-18 18:09:32'),
-(5, '李小華', 'yang@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 17:43:45', '2025-03-18 18:09:38'),
-(6, '張營芳', 'user6@example.com', 'fc4c0d35e3f7d6292f24a95f896f1eae2fdc46cf4a5a8246d4b79b0ee0a5b8c4', NULL, '2025-03-16 17:43:45', '2025-03-16 17:43:45'),
-(7, '黃嚴婷', 'user7@example.com', 'e99a18c428cb38d5f260853678922e03abd8d62b8e18f27f6fc2c17d14e6f0ef', NULL, '2025-03-16 17:43:45', '2025-03-16 17:43:45'),
-(8, '趙禮志', 'user8@example.com', '7a57a5a743894a0e72e525d9ff363b60f7285a6e78bd030ccd54816b7b7a1327', NULL, '2025-03-16 17:43:45', '2025-03-16 17:43:45'),
-(9, '許新麗', 'user9@example.com', 'b89eaac7e61417341b710b727768294d0e6a277b3f3af21c5f6ac1c0f2f66c92', NULL, '2025-03-16 17:43:45', '2025-03-16 17:43:45'),
-(10, '周成宏', 'user10@example.com', '0a7d2c61a8d0745126657a4b7c3d4f305ea3c7f19c3d3e9df99954ef797b6e02', NULL, '2025-03-16 17:43:45', '2025-03-16 17:43:45');
+(1, '陳小明', 'owen@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 09:43:45', '2025-03-18 10:09:15'),
+(2, '林一美', 'jing@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 09:43:45', '2025-03-18 10:09:20'),
+(3, '劉秀杰', 'hsin@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 09:43:45', '2025-03-18 10:09:25'),
+(4, '王名強', 'ching@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 09:43:45', '2025-03-18 10:09:32'),
+(5, '李小華', 'yang@example.com', '$2b$10$Op2jWHwdvd8v.DPYXRov5O.nssk9zdwn61R6bre4vxgsv8Jc/5x.C', NULL, '2025-03-16 09:43:45', '2025-03-18 10:09:38'),
+(6, '張營芳', 'user6@example.com', 'fc4c0d35e3f7d6292f24a95f896f1eae2fdc46cf4a5a8246d4b79b0ee0a5b8c4', NULL, '2025-03-16 09:43:45', '2025-03-16 09:43:45'),
+(7, '黃嚴婷', 'user7@example.com', 'e99a18c428cb38d5f260853678922e03abd8d62b8e18f27f6fc2c17d14e6f0ef', NULL, '2025-03-16 09:43:45', '2025-03-16 09:43:45'),
+(8, '趙禮志', 'user8@example.com', '7a57a5a743894a0e72e525d9ff363b60f7285a6e78bd030ccd54816b7b7a1327', NULL, '2025-03-16 09:43:45', '2025-03-16 09:43:45'),
+(9, '許新麗', 'user9@example.com', 'b89eaac7e61417341b710b727768294d0e6a277b3f3af21c5f6ac1c0f2f66c92', NULL, '2025-03-16 09:43:45', '2025-03-16 09:43:45'),
+(10, '周成宏', 'user10@example.com', '0a7d2c61a8d0745126657a4b7c3d4f305ea3c7f19c3d3e9df99954ef797b6e02', NULL, '2025-03-16 09:43:45', '2025-03-16 09:43:45');
 
 --
 -- 已傾印資料表的索引
