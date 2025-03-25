44c8ce7cbfc0856393487614e0e92c1338444185
PAO <4A61C003@stust.edu.tw>
2025-03-25 22:57:08 +0800

    fav-list、bugfix

--- app/member/_component/fav-list.js
+++ app/member/_component/fav-list.js
@@ -1,12 +1,17 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
-import favListCss from '../_styles/member.module.css'
+import React from 'react'
+import { useSearchParams} from 'next/navigation'
 import Card from '../_component/card'
 
 export default function FavList() {
+  const searchParams = useSearchParams()
+  const category = searchParams.get('category') || '' 
+ 
+
   return (
     <>
+    <h2>{category}</h2>
       <Card />
     </>
   )
