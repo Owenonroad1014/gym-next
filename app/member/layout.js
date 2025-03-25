afed4b0e314aa5bf733e605d5fc2d1feac98d477
PAO <4A61C003@stust.edu.tw>
2025-03-25 20:32:42 +0800

    會員中心頁面-1(會員中心layout、列表卡片樣式)

--- app/member/layout.js
+++ app/member/layout.js
@@ -2,16 +2,21 @@
 
 import React, { useState, useEffect } from 'react'
 import Breadcrumb from './_component/breadcrumb'
- import Select from './_component/select'
-import { useAuth } from '@/context/auth-context'
+import memberCss from './_styles/member.module.css'
+import Select from './_component/select'
+// import { useAuth } from '@/context/auth-context'
 
 export default function MemberLayout({children}) {
     const breadcrumb = ['首頁', '會員中心']
   return (
     <>
     <Breadcrumb breadcrumb={breadcrumb}/>
+    <div className={memberCss.memberContainer} >
     <Select/>
+    <div className={memberCss.memberContent}>
       {children}
+    </div>
+    </div>
     </>
   )
 }
--- app/member/page.js
+++ app/member/page.js
@@ -1,15 +1,37 @@
 'use client'
 
 import React from 'react'
-
-
-// import { useAuth } from '@/context/auth-context'
-
+import Link from 'next/link'
+import memberCss from './_styles/member.module.css'
+import { useAuth } from '@/context/auth-context'
+import Card from './_component/card'
+// import FavList from './_component/fav-list'
 export default function MemberPage() {
-  
-// const {auth, getAuthHeader} =useAuth()
-return <>
-    
-    {/* {auth.id ? ():()} */}
-  </>
+  const { auth, getAuthHeader } = useAuth()
+  return (
+    <>
+      {auth.id ? (
+        <>
+          <div className={memberCss.memberAdmin}><Card/></div>
+        </>
+      ) : (
+        <>
+          <div className={memberCss.memberNoAdmin}>
+            <div className={memberCss.memberSpan}>
+              <h1>您好，請先登入</h1>
+              <span>若您尚未成為會員，請先註冊</span>
+            </div>
+            <div className={memberCss.memberBtns}>
+              <Link className={memberCss.memberBtn} href="/member/login">
+                會員登入
+              </Link>
+              <Link className={memberCss.memberBtn} href="/member/register">
+                註冊會員
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
 }
