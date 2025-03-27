3c723b7e6223773b7ca3f8ded960f0ad9c50f05b
PAO <4A61C003@stust.edu.tw>
2025-03-27 09:45:29 +0800

    頭貼驗證暫時註解

--- utils/schema/schema.js
+++ utils/schema/schema.js
@@ -22,16 +22,16 @@ export const rgSchema = z
 
 export const pfSchema = z
   .object({
-    avatar: z
-      .instanceof(File)
-      .optional()
-      .refine((file) => file.size <= 5 * 1024 * 1024, {
-        message: '頭像大小不能超過5MB',
-      }) // 限制檔案大小
-      .refine(
-        (file) => ['image/jpeg', 'image/png', 'image/webp'].includes(file.type),
-        { message: '只接受 JPEG, PNG 或 WEBP 格式' }
-      ),
+    // avatar: z
+    //   .instanceof(File)
+    //   .optional()
+    //   .refine((file) => file.size <= 5 * 1024 * 1024, {
+    //     message: '頭像大小不能超過5MB',
+    //   }) // 限制檔案大小
+    //   .refine(
+    //     (file) => ['image/jpeg', 'image/png', 'image/webp'].includes(file.type),
+    //     { message: '只接受 JPEG, PNG 或 WEBP 格式' }
+    //   ),
     pname: z
       .string()
       .min(1, { message: '姓名為必填' })
