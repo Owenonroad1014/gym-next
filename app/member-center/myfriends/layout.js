ea4399d770bf804c3ce2a256bda0bbc93587ee1f
jing <n112140@gmail.com>
2025-04-01 14:57:06 +0800

    修改聊天室樣式

---
+++ app/member-center/myfriends/layout.js
@@ -0,0 +1,19 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+import '@/Styles/globals.css'
+import Menubar from './_components/menubar'
+import styles from './_styles/chatLayout.module.css'
+
+export default function ChatLayout({ children }) {
+  return (
+    <div className={styles.chatLayout}>
+      <div className={styles.sidebar}>
+        <Menubar />
+      </div>
+      <div className={styles.messageArea}>
+        {children} {/* 這裡插入訊息區域的內容 */}
+      </div>
+    </div>
+  )
+}
