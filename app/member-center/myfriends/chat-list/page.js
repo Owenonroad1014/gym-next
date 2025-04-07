fbbfd4e1a6d88e0f437d9ad09ac483f8caa311d8
jing <n112140@gmail.com>
2025-04-07 13:25:46 +0800

    新增判斷是否有登入

--- app/member-center/myfriends/chat-list/page.js
+++ app/member-center/myfriends/chat-list/page.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react'
 import chatListStyle from '../_styles/chatList.module.css'
 import { useAuth } from '@/context/auth-context'
 import { CHATS_LIST, GYMFRIEND_AVATAR } from '@/config/api-path'
+import { useRouter } from 'next/navigation'
+
 import Image from 'next/image'
 import moment from 'moment'
 import Link from 'next/link'
@@ -12,6 +14,7 @@ export default function ChatListPage(props) {
   const { auth, getAuthHeader } = useAuth()
   const [chatListData, setChatListData] = useState({})
   const [error, setError] = useState('')
+  const router = useRouter()
 
   useEffect(() => {
     // 獲取聊天室列表
@@ -34,74 +37,78 @@ export default function ChatListPage(props) {
   }, [auth, getAuthHeader])
   return (
     <>
-      <div className={chatListStyle.friendsPage}>
-        <p className={chatListStyle.title}>
-          <IoChatbubbleEllipsesSharp />
-          &nbsp; 聊天室列表
-        </p>
-        <div className={chatListStyle.dash}></div>
-        <ul>
-          {chatListData.totalRows > 0 ? (
-            chatListData?.rows?.map((v, i) => {
-              const msgdate = moment(v.newMsgTime)
-              const today = moment()
-              let formattedDate
-              if (msgdate.isSame(today, 'day')) {
-                formattedDate = msgdate.format('HH:mm')
-              } else {
-                formattedDate = msgdate.format('YYYY/MM/DD')
-              }
-              if (
-                (auth.id == v.user1_id && v.user1_delete == 1) ||
-                (auth.id == v.user2_id && v.user2_delete == 1)
-              ) {
-                return null
-              }
-              return (
-                <li key={v.id}>
-                  <Link href={`/member-center/myfriends/chat-list/${v.id}`}>
-                    <div className={chatListStyle.leftsection}>
-                      <div className={chatListStyle.avatar}>
-                        <Image
-                          src={`${GYMFRIEND_AVATAR}/${
-                            auth.id == v.user1_id
-                              ? v.user2_avatar
-                              : v.user1_avatar
-                          }`}
-                          alt="avatar"
-                          width={35}
-                          height={35}
-                        />
-                      </div>
-                    </div>
-                    <div className={chatListStyle.middlesection}>
-                      <div className={chatListStyle.name}>
-                        {auth.id == v.user1_id ? v.user2_name : v.user1_name}
-                      </div>
-                      <div className={chatListStyle.content}>{v.message}</div>
-                    </div>
-                    <div className={chatListStyle.rightSection}>
-                      <div className={chatListStyle.rightTopSection}>
-                        {formattedDate}
-                      </div>
-                      {v.unread_count != 0 ? (
-                        <div className={chatListStyle.rightBottomSection}>
-                          {v.unread_count}
+      {auth.id ? (
+        <div className={chatListStyle.friendsPage}>
+          <p className={chatListStyle.title}>
+            <IoChatbubbleEllipsesSharp />
+            &nbsp; 聊天室列表
+          </p>
+          <div className={chatListStyle.dash}></div>
+          <ul>
+            {chatListData.totalRows > 0 ? (
+              chatListData?.rows?.map((v, i) => {
+                const msgdate = moment(v.newMsgTime)
+                const today = moment()
+                let formattedDate
+                if (msgdate.isSame(today, 'day')) {
+                  formattedDate = msgdate.format('HH:mm')
+                } else {
+                  formattedDate = msgdate.format('YYYY/MM/DD')
+                }
+                if (
+                  (auth.id == v.user1_id && v.user1_delete == 1) ||
+                  (auth.id == v.user2_id && v.user2_delete == 1)
+                ) {
+                  return null
+                }
+                return (
+                  <li key={v.id}>
+                    <Link href={`/member-center/myfriends/chat-list/${v.id}`}>
+                      <div className={chatListStyle.leftsection}>
+                        <div className={chatListStyle.avatar}>
+                          <Image
+                            src={`${GYMFRIEND_AVATAR}/${
+                              auth.id == v.user1_id
+                                ? v.user2_avatar
+                                : v.user1_avatar
+                            }`}
+                            alt="avatar"
+                            width={35}
+                            height={35}
+                          />
                         </div>
-                      ) : (
-                        ''
-                      )}
-                    </div>
-                  </Link>
-                </li>
-              )
-            })
-          ) : (
-            <p className={chatListStyle.nofriReq}>目前沒有好友...</p>
-          )}
-        </ul>
-        <br />
-      </div>
+                      </div>
+                      <div className={chatListStyle.middlesection}>
+                        <div className={chatListStyle.name}>
+                          {auth.id == v.user1_id ? v.user2_name : v.user1_name}
+                        </div>
+                        <div className={chatListStyle.content}>{v.message}</div>
+                      </div>
+                      <div className={chatListStyle.rightSection}>
+                        <div className={chatListStyle.rightTopSection}>
+                          {formattedDate}
+                        </div>
+                        {v.unread_count != 0 ? (
+                          <div className={chatListStyle.rightBottomSection}>
+                            {v.unread_count}
+                          </div>
+                        ) : (
+                          ''
+                        )}
+                      </div>
+                    </Link>
+                  </li>
+                )
+              })
+            ) : (
+              <p className={chatListStyle.nofriReq}>目前沒有好友...</p>
+            )}
+          </ul>
+          <br />
+        </div>
+      ) : (
+        router.push('/member-center')
+      )}
     </>
   )
 }
--- app/member-center/myfriends/page.js
+++ app/member-center/myfriends/page.js
@@ -2,13 +2,12 @@
 
 import React, { useState, useEffect } from 'react'
 import FriendListPage from './_components/friend-list'
-import styles from './_styles/chatList.module.css'
-import Link from 'next/link'
-import { LiaUserFriendsSolid } from 'react-icons/lia'
+import { useAuth } from '@/context/auth-context'
+import { useRouter } from 'next/navigation'
+
 export default function ChatPage() {
-  return (
-    <>
-      <FriendListPage />
-    </>
-  )
+  const { auth, getAuthHeader } = useAuth()
+  const router = useRouter()
+
+  return <>{auth.id ? <FriendListPage /> : router.push('/member-center')}</>
 }
